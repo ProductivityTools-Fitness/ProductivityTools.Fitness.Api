e7badcdd0ce42f5a58b77007e780166b5a165bb6
Pawel Wujczyk <pwujczyk@gmail.com>
2026-07-16 18:36:29 +0200

    controllers

--- src/main/java/top/productivitytools/fitness/api/controllers/ScheduleController.java
+++ src/main/java/top/productivitytools/fitness/api/controllers/ScheduleController.java
@@ -1,5 +1,10 @@
 package top.productivitytools.fitness.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedule")
 public class ScheduleController {
     
 }
