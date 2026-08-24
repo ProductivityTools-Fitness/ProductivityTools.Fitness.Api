00b961b38e7f992042450db2d8f71b0d90fb9978
Pawel Wujczyk <pwujczyk@gmail.com>
2026-08-24 21:08:10 +0000

    rebase

---
+++ src/main/java/top/productivitytools/fitness/api/configuration/CorsConfig.java
@@ -0,0 +1,21 @@
+package top.productivitytools.fitness.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://service-status.productivitytools.top",
+                        "https://pt-service-status-93484780890.us-central1.run.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
