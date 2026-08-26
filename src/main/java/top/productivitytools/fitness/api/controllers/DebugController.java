910ab33c4300e93380bd8be5634d0883de7c664f
Pawel Wujczyk <pwujczyk@gmail.com>
2026-08-26 17:34:59 +0000

    date format

--- src/main/java/top/productivitytools/fitness/api/controllers/DebugController.java
+++ src/main/java/top/productivitytools/fitness/api/controllers/DebugController.java
@@ -4,23 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequestMapping("/api/debug")
 public class DebugController {
-    
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
     @GetMapping("/hello")
     public String Hello() {
         return "Hello";
     }
 
-    
     @GetMapping("/appName")
     public String AppName() {
         return "PTFitness";
     }
-    
+
     @GetMapping("/date")
     public String Date() {
-        return new java.util.Date().toString();
+        return LocalDateTime.now().format(DATE_TIME_FORMATTER);
     }
 }
